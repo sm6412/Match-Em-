d1f054cc556103bf15741a6dbdb0d143a3ea2a8c
Samira Mantri <sm6412@nyu.edu>
2019-05-10 20:15:17 -0400

    put a custom cursor

---
+++ Assets/Scripts/Start Scene Scripts/MouseCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+
+    // make new cursor follow mouse 
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = cursorPos;
+
+        
+    }
+}
