55f0cae70e4ca1cab002efac1f54d776e080e943
Samira Mantri <sm6412@nyu.edu>
2019-05-11 10:19:23 -0400

    updated scripts

---
+++ Assets/Scripts/Start Scene Scripts/ExitController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// create script that allows you to exit the game at any point
+public class ExitController : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        
+    }
+}
